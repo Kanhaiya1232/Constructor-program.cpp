2070aaebfde52440441fc000191105388cddc381
Kanhaiya1232 <101667615+Kanhaiya1232@users.noreply.github.com>
2022-04-12 19:28:16 +0530

    Add files via upload

---
+++ constructor program.cpp
@@ -0,0 +1,16 @@
+//Constructor program to pring Hello
+#include<iostream>
+using namespace std;
+class A
+{
+    public:
+    A()
+    {
+        cout<<"Hello constructor";
+    }
+};
+int main()
+{
+    A obj;
+    return 0;
+}
